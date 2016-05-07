ee9052dc172ff2105777f251c847b67492cae723
JENK <twitchymage@users.noreply.github.com>
2016-05-07 10:35:51 -0700

    Functions added
    Saving my progress before something explodes. Dots still don't do
    anything much but they are stored in an array now and functions exist
    that can work on an array of dots.

--- myGame/_script/myGrid.js
+++ myGame/_script/myGrid.js
@@ -10,6 +10,8 @@ main.className = "ui-grid-b gamegrid center";
     //Nests this <div> in the <div> referred to by the var "container"
 container.appendChild(main);
 
+    var dotArray = [];
+    
     //Creates 9 button-containing objects
 for(var i = 0; i < 9; i++) {
     var gridElement = document.createElement("div");
@@ -25,15 +27,45 @@ for(var i = 0; i < 9; i++) {
     
     var newDot = document.createElement("div");
     newDot.className = "dot";
+    newDot.id = "dot" + i;
     gridElement.appendChild(newDot);
-}
-    
-    /*    
-    //Add custom properties to objects
-    
-    aButton.setAttribute('isVisited',false);*/
 
-    //Function responsible for tap-responsiveness
+    //Add custom properties to objects
+    newDot.setAttribute('isVisited','false');
+    
+    //Add each newDot to the array
+    dotArray[i] = newDot;
+}
+    //Testing each function
+    document.getElementById("towrite").innerHTML = (dotArray.length) + " Dots " +  toStringIds(dotArray) + ifVisited(getIsVisited(dotArray[0]));
+    
+    //Returns whether a dot has been visited
+    function getIsVisited(element) {
+        return element.getAttribute('isVisited');
+    }
+    
+    //String comparison: OH MY GOD IT FINALLY WORKED
+    function ifVisited(element) {
+        return element=='false';
+    }
+    
+    //Returns the IDs of each dot in an array
+    function toStringIds(arr) {
+        var i;
+        var str = "";
+        for(i=0;i<arr.length;i++){
+            str = str.concat(arr[i].id + " ");
+        }
+        return str;
+    }
+    
+    //TODO
+    //Function that generates path that the user must replicate
+    //Illuminate each dot in this game-generated path in sequence
+    //Store each dot clicked in an array
+    //Compare the user-made path with the game-generated path. Whenever there is deviation, inform the user right away and move on to the next path
+
+    //Function responsible for changing the dot's colour when tapped
     $(function(){
         $( ".dot" ).bind( "tap", tapHandler );
         function tapHandler( event ){
