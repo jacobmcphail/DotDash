c4dca2dc7995caffe9d7b17c310bce73aa267a99
JENK <twitchymage@users.noreply.github.com>
2016-05-07 13:50:01 -0700

    3x3 only, but it's a start. TAKE A LOOK.
    -Generates a 3x3 grid
    -Creates a 2D array of dots
    -Populates the grid with dots
    -By default each dot object has isVisited property = false

--- myGame/_script/myGrid.js
+++ myGame/_script/myGrid.js
@@ -1,4 +1,11 @@
 /*mygrid.js*/
+
+/*Issues to fix:
+-Generating grids of any size (more than 3x3)
+-Creating a flexible container that accommodates grids of different sizes (right now CSS is formatted so that 3x3 displays nicely with my browser. Anything else and it turns to poop.)
+-Can't put the code that appends dots to grid cells in a separate function
+-
+*/
 window.onload = function(){
     
 var container = document.getElementById("wrapper");
@@ -8,87 +15,23 @@ main.className = "ui-grid-b gamegrid center";
 container.appendChild(main);
 
     var dotArray = [];
+    var numRows = 3;
+    var numCols = 3;
     
-    /*Separate out into a different function that takes row and column sizes as arguments
-    As of now, this function assumes numCols is always going to be 3
-    */
+    main = createGrid(main,numRows,numCols);
+    dotArray = make_2D_Array(dotArray,numRows,numCols);            
+    /*Attempt 2: not creating a separate function for code that populates grids with dots. THIS WORKS   */
     
-    function createGrid(cont,numRows,numCols){
-        
-    var totalDots = numRows * numCols;
-    for(var i = 0; i < totalDots; i++) {
-    var gridElement = document.createElement("div");
-    //How to accommodate numCols >3
-        if(i%numCols==0){
-            gridElement.className = "ui-block-a box";
-        } else if (i%numCols==1) {
-            gridElement.className = "ui-block-b box";
-        } else if (i%numCols==2) {
-            gridElement.className = "ui-block-c box";
-        }
-        gridElement.id = "block"+i;
-        
-        //Takes a container to hold the grid in as an argument and returns the updated grid.
-        cont.appendChild(gridElement);
-        
-        }
-        return cont;
-    }
-    
-
-    main = createGrid(main,3,3);
-    
-    //Creates 9 button-containing objects
-    //Fill up each array with an array
-    dotArray = make_2D_Array(dotArray,3,3);
-
-    function make_2D_Array(array, nRows, nCols) {
-        var i, j;
-        
-        for(i=0;i<nRows;i++){
-            var newArray = [];
-            array[i] = newArray;
-            for(j=0;j<nCols;j++){
-        var newDot = document.createElement("div");
-        newDot.className = "dot";
-        newDot.id = "dot" + (i*j);
-                newArray[j] = newDot;
-                
+    var dotID = 1;
+    for(var i=0;i<numRows;i++){
+            for(var j=0;j<numCols;j++){ document.getElementById("block"+dotID).appendChild(dotArray[i][j]);
+                dotID++;
             }
-        }
-        return array;
     }
     
-    //Testing make_2D_Array function
-    document.getElementById("towrite").innerHTML = (dotArray.length) + " Dots ";
+    //Testing custom property
+    //document.getElementById("towrite").innerHTML =  getIsVisited(dotArray[0][0]);
     
-    
-/*Function call would look like fill_grid(dotArray,main);*/    
-    function fill_Grid(array, mainDiv){
-        var numRows = array.length;
-        var numCols = array[0].length;
-        var i, j;
-        for(i=0;i<numRows;i++){
-            for(j=0;j<numCols;j++){
-                var dotToAdd = array[i][j];
-                
-                
-                //SAVEPOINT: 12:56 Nothing broken yet and not done this at all. Need to getElements by Id in order to assign the right dots to the right grid elements. 
-            }
-        }
-        
-    }
-
-    //Add custom properties to objects
-    //newDot.setAttribute('isVisited','false');
-    
-    
-    //TODO
-    //Function that generates path that the user must replicate
-    //Illuminate each dot in this game-generated path in sequence
-    //Store each dot clicked in an array
-    //Compare the user-made path with the game-generated path. Whenever there is deviation, inform the user right away and move on to the next path
-
     //Function responsible for changing the dot's colour when tapped
     $(function(){
         $( ".dot" ).bind( "tap", tapHandler );
@@ -98,6 +41,62 @@ container.appendChild(main);
     });
     
 }
+
+
+    /*Create the grid that dots will populate
+    As of now, this function assumes numCols is always going to be 3
+    */
+    
+    function createGrid(cont,numRows,numCols){
+        
+    var totalDots = numRows * numCols;
+    var dotID = 1;
+    for(var i = 0; i < totalDots; i++) {
+        var gridElement = document.createElement("div");
+    //How to accommodate numCols >3
+        if(i%numCols==0){
+            gridElement.className = "ui-block-a box";
+        } else if (i%numCols==1) {
+            gridElement.className = "ui-block-b box";
+        } else if (i%numCols==2) {
+            gridElement.className = "ui-block-c box";
+        }
+        
+        gridElement.id = "block"+(dotID);
+        dotID++;
+        
+        //Takes a container to hold the grid in as an argument and returns the updated grid.
+        cont.appendChild(gridElement);
+        
+        }
+        return cont;
+    }
+
+/*Creates 9 button-containing objects
+    Fill up each array with an array*/
+    function make_2D_Array(array, nRows, nCols) {
+        var i, j;
+        var dotID = 1;
+        
+        for(i=0;i<nRows;i++){
+            var newArray = [];
+            array[i] = newArray;
+            for(j=0;j<nCols;j++){
+        var newDot = document.createElement("div");
+        newDot.className = "dot";
+        newDot.id = "dot" + dotID;
+                dotID++;
+                newArray[j] = newDot;
+                //Add custom properties to objects
+        newDot.setAttribute('isVisited','false');
+    
+                
+            }
+        }
+        return array;
+    }
+    
+
 
 //Returns whether a dot has been visited
     function getIsVisited(element) {
@@ -119,3 +118,9 @@ container.appendChild(main);
         return str;
     }
     
+
+//TODO
+    //Function that generates path that the user must replicate
+    //Illuminate each dot in this game-generated path in sequence
+    //Store each dot clicked in an array
+    //Compare the user-made path with the game-generated path. Whenever there is deviation, inform the user right away and move on to the next path
