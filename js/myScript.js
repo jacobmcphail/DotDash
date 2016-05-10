61143f6b0b23e261f232b0d83c482a252a127309
Jerky One <solitsea@opmbx.org>
2016-05-10 10:05:17 -0700

    10:04 SavePoint
    Last push to master before branching off

--- js/myScript.js
+++ js/myScript.js
@@ -1,124 +1,129 @@
 /*mygrid.js*/
-window.onload = function(){
-//Container that holds everything.
-var container = document.getElementById("wrapper");
-//Array that holds references to all the dot-containing div elements
-var dotArray = [];
-//Point constructor
-function Point(x,y){
-this.x = x;
-this.y = y;
-}
-//Maximum number of rows: 5
-var numRows = 3;
-//Maximum number of columns: 4
-var numCols = 3;
-//Grid of div containers. Each one will hold a single dot-containing div element.
-var main = document.createElement("div");
-if (numCols==3){
-main.className = "ui-grid-b gamegrid center";
-}
-else if(numCols==4){
-main.className = "ui-grid-c gamegrid center";
-}
-container.appendChild(main);
-main = createGrid(main,numRows,numCols);
-dotArray = make_2D_Array(dotArray,numRows,numCols);
-/* Create div elements containing dots and populate main with them.
-Fill dotArray with these dot-containing divs. */
-var dotID = 1;
-for(var i=0;i<numRows;i++){
-for(var j=0;j<numCols;j++){
-document.getElementById("block"+dotID).appendChild(dotArray[i][j]);
-dotID++;
-}
-}
+
+window.onload = function() {
+    //Container that holds everything.
+    var container = document.getElementById("wrapper");
+    //Array that holds references to all the dot-containing div elements
+    var dotArray = [];
+    //Point constructor
+    function Point(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    //Maximum number of rows: 5
+    var numRows = 3;
+    //Maximum number of columns: 4
+    var numCols = 3;
+    //Grid of div containers. Each one will hold a single dot-containing div element.
+    var main = document.createElement("div");
+    if (numCols==3){
+        main.className = "ui-grid-b gamegrid center";
+    }
+    else if(numCols==4){
+        main.className = "ui-grid-c gamegrid center";
+    }
+    container.appendChild(main);
+    main = createGrid(main,numRows,numCols);
+    dotArray = make_2D_Array(dotArray,numRows,numCols);
+    /* Create div elements containing dots and populate main with them.
+    Fill dotArray with these dot-containing divs. */
+    var dotID = 1;
+    for(var i=0;i<numRows;i++){
+        for(var j=0;j<numCols;j++){
+            document.getElementById("block"+dotID).appendChild(dotArray[i][j]);
+            dotID++;
+        }
+    }
 //----------------------------------------------
 //Game playing field is instantiated at this point
 //Round start: (nest inside an infinite loop)
 //function createPath(){ return arrayToRepeat;} goes here
 //function displayPath(arrayToRepeat) goes here
-//********PATH VALIDATION BEGINS HERE*****************
-var noErrorsYet = true;
-var notComplete = true;
-var index = 0;
-/*Hardcode a sample array of points that this function
-should compare user input to.
-Once createPath() finished, this compares user input to
-output of createPath()
-*/
-var arrayToRepeat = [];
-arrayToRepeat.push(new Point(0,0));
-arrayToRepeat.push(new Point(0,1));
-arrayToRepeat.push(new Point(2,2));
-arrayToRepeat.push(new Point(2,0));
-console.log(arrayToRepeat);
-//Anonymous path validation function goes here
-/* Any dot that is selected by the user has its x and y coordinates compared to
-* x and y coordinates of successive points in arrayToRepeat
-* Comparison takes place every time a dot is selected
-* If the coordinates do not match, output something that is taken as an argument in
-* feedback(boolean) */
-$(document).ready(function(){
-var ex, wai;
-$(".dot").on("tap",function(){
-console.log("Tapped: "+ noErrorsYet+", " + notComplete + ", " + index);
-ex = $(this).attr("x");
-wai = $(this).attr("y");
-if(noErrorsYet&&notComplete){
-if(ex==arrayToRepeat[index].x && wai==arrayToRepeat[index].y){
-if(index<arrayToRepeat.length){
-console.log("Correct so far " + index);
-index++;
-if(index>=arrayToRepeat.length){
-console.log("All correct!");
-notComplete = false;
-}
-}
-}else{
-console.log("Incorrect");
-noErrorsYet = false;
-}
-//Not necessary in final version. Just for testing.
-} else if (!noErrorsYet) {
-console.log("You done goofed and can't undo it");
-}
-});
-});
-//function feedback(boolean) goes here
-/*The boolean return value of recordUserInput is used by
-* function feedback(boolean)
-* and turns the entire grid either green (user got it right) if true
-* or red (user done goofed) if false*/
-/*function clearGrid()
-* delete all the child elements of container with the JQuery method empty()*/
-//Function responsible for changing the dot's colour when tapped
-$(function(){
-$( ".dot" ).bind( "tap", tapHandler );
-function tapHandler( event ){
-if($(event.target).hasClass("selected")){
-$( event.target ).removeClass( "selected" );
-} else {
-$( event.target ).addClass( "selected" );
-}
-}
-});
-$(function(){
-$(".clearButton").bind("tap", clearAll);
-function clearAll(event){
-$(".dot.selected").removeClass("selected");
-$(".dot.tapped_steve").addClass("steve");
-$(".dot.tapped_steve").removeClass("tapped_steve");
-console.clear();
-resetVals();
-}
-//helper function that resets things
-function resetVals(){
-noErrorsYet = true;
-notComplete = true;
-index = 0;
-}
-});
+    //********PATH VALIDATION BEGINS HERE*****************
+    var noErrorsYet = true;
+    var notComplete = true;
+    var index = 0;
+    /*Hardcode a sample array of points that this function
+    should compare user input to.
+    Once createPath() finished, this compares user input to
+    output of createPath()
+    */
+    var arrayToRepeat = [];
+    arrayToRepeat.push(new Point(0,0));
+    arrayToRepeat.push(new Point(0,1));
+    arrayToRepeat.push(new Point(2,2));
+    arrayToRepeat.push(new Point(2,0));
+    console.log(arrayToRepeat);
+
+    //Anonymous path validation function goes here
+    /* Any dot that is selected by the user has its x and y coordinates compared to
+    * x and y coordinates of successive points in arrayToRepeat
+    * Comparison takes place every time a dot is selected
+    * If the coordinates do not match, output something that is taken as an argument in
+    * feedback(boolean) */
+    $(document).ready(function(){
+        var ex, wai;
+        $(".dot").on("tap",function(){
+            console.log("Tapped: "+ noErrorsYet+", " + notComplete + ", " + index);
+            ex = $(this).attr("x");
+            wai = $(this).attr("y");
+
+            if(noErrorsYet&&notComplete){
+                if(ex==arrayToRepeat[index].x && wai==arrayToRepeat[index].y){
+                    if(index<arrayToRepeat.length){
+                        console.log("Correct so far " + index);
+                        index++;
+                        if(index>=arrayToRepeat.length){
+                            console.log("All correct!");
+                            notComplete = false;
+                        }
+                    }
+                }else{
+                    console.log("Incorrect");
+                    noErrorsYet = false;
+                }
+            //Not necessary in final version. Just for testing.
+            } else if (!noErrorsYet) {
+                console.log("You done goofed and can't undo it");
+            }
+        });
+    });
+    //function feedback(boolean) goes here
+    /*The boolean return value of recordUserInput is used by
+    * function feedback(boolean)
+    * and turns the entire grid either green (user got it right) if true
+    * or red (user done goofed) if false*/
+    /*function clearGrid()
+    * delete all the child elements of container with the JQuery method empty()*/
+    //Function responsible for changing the dot's colour when tapped
+    $(function(){
+        $( ".dot" ).bind( "tap", tapHandler );
+        function tapHandler( event ){
+            if($(event.target).hasClass("selected")){
+                $( event.target ).removeClass( "selected" );
+            } else {
+                $( event.target ).addClass( "selected" );
+            }
+        }
+    });
+
+    $(function(){
+        $(".clearButton").bind("tap", clearAll);
+        function clearAll(event){
+            $(".dot.selected").removeClass("selected");
+            $(".dot.tapped_steve").addClass("steve");
+            $(".dot.tapped_steve").removeClass("tapped_steve");
+            console.clear();
+            resetVals();
+        }
+    //helper function that resets things
+        function resetVals(){
+        noErrorsYet = true;
+        notComplete = true;
+        index = 0;
+        }
+    });
+
 }
 //End of the function called by window.onload
 //Turns all dots into Steve heads. If you tap them, they turn angry. You can't untap them.
