1a269d7933d31987468bc9ad0becc27d32015067
teahelaschuk <teahelaschuk@gmail.com>
2016-05-17 12:12:03 -0700

    play again button working

--- js/jerkScript.js
+++ js/jerkScript.js
@@ -25,6 +25,8 @@ var numCols = 3;
 var localSavedFiles;
 var container;
 var dotArray = [];
+var highscores = ["-----", "scrumcake", "-------", "-------", "-------", "-------", "-------", "-------", "-------"];
+var mode;
 
 var colourArray = ["cyan","orange","green","pink","blue","purple"];
 var colour;
@@ -39,30 +41,14 @@ $.getScript("js/pathGenerator.js", function(){
 
 //File storing function
 function gameSetup() {
-	checkCookie();
 	localSavedFiles = JSON.parse(localStorage.getItem("saveFile"));
 	if (localSavedFiles == null) {
-		localSavedFiles = [false, false, false, false, false, false, false, false, false, false, false, 0, 0, 0];
+		localSavedFiles = [false, false, false, false, false, false, false, false, false, false, false];
 		localStorage.setItem("saveFile", JSON.stringify(localSavedFiles));
 	} else {
 		console.log(localSavedFiles);
 		updateBadges();
 	}
-	updateHighScores();
-}
-
-function checkCookie(){
-    var cookieEnabled=(navigator.cookieEnabled)? true : false;
-    if (typeof navigator.cookieEnabled=="undefined" && !cookieEnabled){ 
-        document.cookie="testcookie";
-        cookieEnabled=(document.cookie.indexOf("testcookie")!=-1)? true : false;
-    }
-    return (cookieEnabled)?true:showCookieFail();
-}
-
-function showCookieFail(){
-  console.log("NO COOKIES");
-  window.alert("WARNING: Cookies must be enabled to play this game. Enable cookies then refresh the game.");
 }
 
 function clearSave() {
@@ -71,15 +57,16 @@ function clearSave() {
 	gameSetup();
 }
 
-    //Update high scores
-function updateHighScores() {
-    var scoreSpaces = document.getElementsByClassName("score-text");
-    for (var i = 11, q = 0; i < 14; i++, q++) {
-        scoreSpaces[q].innerHTML = localSavedFiles[i];
-    }
-}
-
 $(document).ready(function(){
+    updateHighScores();
+
+    //Update high scores
+    function updateHighScores() {
+        var scoreSpaces = document.getElementsByClassName("score-text");
+        for (var i = 0; i < highscores.length; i++) {
+            scoreSpaces[i].innerHTML = highscores[i];
+        }
+    }
 
     //return scores to normal after Steve Mode is disabled
     $("#option-4").on('tapone', function(){
@@ -87,12 +74,14 @@ $(document).ready(function(){
             updateHighScores();
         }
     });
-    
+
+
+    // T -- changed from initialize(mode, newround..)
     $(".mode-select").on('tapone', function(){
-        var mode = this.id;
+        //mode = this.id;
 		gamemode = 1; //sets gamemode
         console.log("mode selected: " + mode);
-        initialize(mode, newRound, removeDots);
+        initialize(gamemode, newRound, removeDots);
     });
 	
 	$(".cawButton").on('tapone', function(){
@@ -112,6 +101,8 @@ $(document).ready(function(){
         }
     });
 });
+
+
 
 function initialize(gameMode, newRound, removeDots){
     //Initialize global variables depending on mode (currently only one mode)
@@ -482,12 +473,17 @@ function resetGrid(){
     openMainMenu();
 }
 
+/* resets the level -- assumes there is only one mode */
+function playAgain() {
+    removeDots();
+    resetVals();
+    play();
+}
+
 function gameOver() {
     playing = false;
 	badgeChecker(currentRound, lifePoints);
-	scoreChecker(playerScore);
 	localStorage.setItem("saveFile", JSON.stringify(localSavedFiles));
-	updateHighScores();
     $( "#game-screen" ).fadeOut( 1500, function() {
         $('#gameover-screen').fadeIn(1500, function() {});
     });
@@ -533,34 +529,6 @@ function steveTap(event) {
             $(event.target).addClass("tapped_steve");
         }
     }
-}
-
-function scoreChecker(playerScore) {
-	switch(gamemode) {
-		case 0:
-			if (playerScore > localSavedFiles[11]) {
-				localSavedFiles[12] = playerScore;
-				return true;
-			}
-			break;
-			break;
-		case 1: 
-			if (playerScore > localSavedFiles[12]) {
-				localSavedFiles[12] = playerScore;
-				return true;
-			}
-			break;
-		case 2:
-			if (playerScore > localSavedFiles[13]) {
-				localSavedFiles[12] = playerScore;
-				return true;
-			}
-			break;
-			break;
-		default:
-			window.alert("YOU SHOULD NOT SEE THIS!");
-	}
-	return false;
 }
 
 // Checks to see if player has unlocked any new badges
