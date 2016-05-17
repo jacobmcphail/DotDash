52c67aa767987013329462a9496c30230cfe51a3
Jacob Mcphail <jacobmcphail@gmail.com>
2016-05-17 11:42:21 -0700

    Mobile credit screen fix
    Fixed the credit screen so it fits the mobile screen, fix is a temporary
    one.

--- js/nuggetScript.js
+++ js/nuggetScript.js
@@ -25,7 +25,7 @@ function openCredits() {
     document.getElementById('main-screen').style.display = 'none';
     document.getElementById('credits-screen').style.display = "block";
     document.getElementById('return-button').style.display = "block";
-    document.getElementById('title').innerHTML = "DotDash._"
+    document.getElementById('title').innerHTML = ""
 }
 function openMainMenu() {
     document.getElementById('options-screen').style.display = 'none';
