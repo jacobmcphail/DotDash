278c0b4df6a434acf48122f132316b299bf79929
Jacob Mcphail <jacobmcphail@gmail.com>
2016-05-18 10:51:20 -0700

    Swiping for Mobile (WIP)
    Swiping for Mobile (WIP)

--- js/carousel.js
+++ js/carousel.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
 	// Activate Carousel	
 	$("#myCarousel").carousel("pause");				   
 	// Enable Carousel Controls
-	$(".left").click(function(){
+	$(".left").on('tapone', function(){
 	$("#myCarousel").carousel("prev");
 	});
-	$(".right").click(function(){
+	$(".right").on('tapone', function(){
+	$("#myCarousel").carousel("next");
+	});
+	$("#mode-selection-container").on('swipeleft', function(){
+	$("#myCarousel").carousel("prev");
+	});
+	$("#mode-selection-container").on('swiperight', function(){
 	$("#myCarousel").carousel("next");
 	});
 });
