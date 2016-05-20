17140b1deaee772198a57c14723ae0e27418c03e
xeea <xeea.minhas@gmail.com>
2016-05-20 16:39:16 -0700

    main menu sound fix

--- js/nuggetScript.js
+++ js/nuggetScript.js
@@ -35,6 +35,7 @@ function openCredits() {
 	yesSound.play();
 }
 function openMainMenu() {
+	yesSound.play();
     document.getElementById('options-screen').style.display = 'none';
     document.getElementById('scores-screen').style.display = 'none';
     document.getElementById('credits-screen').style.display = 'none';
