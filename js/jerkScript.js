47f548dacdc1a212f308865e58bf7ee467517ec0
Jerky One <solitsea@opmbx.org>
2016-05-26 10:10:57 -0700

    jerkScript: resetSave()
    -Should be able to clear data in the Daterberse

--- js/jerkScript.js
+++ js/jerkScript.js
@@ -119,6 +119,10 @@ function resetSave() {
     localSavedFiles = [true, null, null];
 	playerData = [false, false, false, false, false, false, false, false, false, false, 0, 0, 0];
 	localStorage.setItem("saveFile", JSON.stringify(localSavedFiles));
+	//@@@MAKING IT SO RESET SAVE CLEARS DATABASE TOO
+	sendSaveData(localSavedFiles[1], localSavedFiles[2]);
+	updateSave();
+	
 	updateBadges();
 	updateHighScores();	
 
